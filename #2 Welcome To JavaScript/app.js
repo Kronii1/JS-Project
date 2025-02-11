f573b694089532e40cb309220c3e5638b88747db
Kronii1 <bfvkdlxj@naver.com>
2025-02-11 23:08:09 +0900

    Add Content README File

---
+++ #2 Welcome To JavaScript/app.js
@@ -0,0 +1,8 @@
+const amICute = true;
+console.log(amICute);
+
+let amIFat = null;
+console.log(amIFat);
+
+let something;
+console.log(something);
--- README.md
+++ README.md
@@ -4,4 +4,5 @@ This repository was created to study HTML & SCSS & JavaScript. 💖
 
 ## 📖 공부기간
 
+DeadLine 02.16
 25.02.05 ~ 25.02.~~
