f80abfd67b5790c0df4d19e23084d70579978363
Kronii1 <bfvkdlxj@naver.com>
2025-02-16 11:18:29 +0900

    #2.8 Functions part Two After Study

--- #2 Welcome To JavaScript/app.js
+++ #2 Welcome To JavaScript/app.js
@@ -1,5 +1,11 @@
-function sayHello() {
-  console.log("Hello");
+function sayHello(name, age) {
+  console.log("Hello my name is " + name + "and i'm " + age);
 }
 
-sayHello("Kronii");
+sayHello("Kronii", 314);
+
+function plus(a, b) {
+  console.log(a + b);
+}
+
+plus(1, 2);
